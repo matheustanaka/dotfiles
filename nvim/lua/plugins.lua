e89be5c8148a0ba869e87be670a08ce845e60082
Matheus Tanaka <matheustanakanog@gmail.com>
2023-12-08 17:35:15 +0000

    update nvim config

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,105 @@
+-- plugins.lua 
+
+return {
+    {
+        'numToStr/Comment.nvim',
+        opts = {
+            -- add any options here
+        },
+        lazy = false,
+    },
+    -- Bufferline
+    {
+        'akinsho/bufferline.nvim',
+        dependencies = {
+            'nvim-tree/nvim-web-devicons'
+        },
+    },
+    -- Auto Pairs
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        opts = {} -- this is equalent to setup({}) function    -- Colorscheme
+    },
+    -- Colorscheme theme
+    { 
+        "EdenEast/nightfox.nvim" 
+    },
+    { 
+        'rose-pine/neovim', name = 'rose-pine'
+    },
+    -- Hop (Better Navigation)
+    {
+        "phaazon/hop.nvim",
+        lazy = true,
+    },
+
+
+    -- Lualine
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = {
+            'nvim-tree/nvim-web-devicons'
+        },
+    },
+    -- Language Support
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        dependencies = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {'williamboman/mason.nvim'},           -- Optional
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},         -- Required
+            {'hrsh7th/cmp-nvim-lsp'},     -- Required
+            {'hrsh7th/cmp-buffer'},       -- Optional
+            {'hrsh7th/cmp-path'},         -- Optional
+            {'saadparwaiz1/cmp_luasnip'}, -- Optional
+            {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},             -- Required
+            {'rafamadriz/friendly-snippets'}, -- Optional
+        }
+    },
+
+    -- Nvimtree (File Explorer)
+    {
+        'nvim-tree/nvim-tree.lua',
+        lazy = true,
+        dependencies = {
+            'nvim-tree/nvim-web-devicons',
+        },
+        config = function ()
+            require("nvim-tree").setup {}
+        end,
+    },
+    -- Telescope (Fuzzy Finder)
+    {
+        'nvim-telescope/telescope.nvim',
+        lazy = true,
+        dependencies = {
+            {'nvim-lua/plenary.nvim'},
+        }
+    },
+    -- Toggle Terminal
+    {
+        'akinsho/toggleterm.nvim',
+        version = "*",
+        config = true
+    },
+    -- Treesitter
+    {
+        "nvim-treesitter/nvim-treesitter",
+    },
+
+    -- Which-key
+    {
+        'folke/which-key.nvim',
+        lazy = true,
+    },
+
+}
