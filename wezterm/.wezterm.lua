89208f21958372298781aee5eb629c8945d873fa
Matheus Tanaka <matheustanakanog@gmail.com>
2024-05-12 14:44:11 -0300

    increase font size and add copy-mode support for tmux

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,22 +7,21 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.font = wezterm.font'Jet Brains Mono Nerd Font'
-config.font_size = 12.0
-config.color_scheme = 'carbonfox'
+config.font = wezterm.font("Jet Brains Mono Nerd Font")
+config.font_size = 15.0
+config.color_scheme = "carbonfox"
 config.enable_tab_bar = false
 config.window_padding = {
-    left = 0,
-    right = 0,
-    bottom = 0,
+	left = 0,
+	right = 0,
+	bottom = 0,
 }
 
 -- and finally, return the configuration to wezterm
 return config
-
