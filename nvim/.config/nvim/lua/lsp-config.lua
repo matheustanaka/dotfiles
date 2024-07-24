9ebb79bc38cc55d5bee0f10654ea4be2477102f4
Matheus Tanaka <matheustanakanog@gmail.com>
2024-07-24 09:02:48 -0300

    create symlink with stow for nvim

---
+++ nvim/.config/nvim/lua/lsp-config.lua
@@ -0,0 +1,52 @@
+-- lsp-config.lua
+
+-- Learn the keybindings, see :help lsp-zero-keybindings
+-- Learn to configure LSP servers, see :help lsp-zero-api-showcase
+local lsp = require('lsp-zero')
+lsp.preset('recommended')
+
+lsp.ensure_installed({
+    'tsserver',
+    'rust_analyzer',
+    'astro',
+    'lua_ls'
+})
+-- (Optional) Configure lua language server for neovim
+lsp.nvim_workspace()
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select), -- previous suggestions
+    ['<C-j>'] = cmp.mapping.select_next_item(cmp_select), -- next suggestions
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(), -- Show completion suggestions
+    ['<C-e>'] = cmp.mapping.abort(), -- Close completion window
+    ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Press enter confirm the selection
+})
+
+
+cmp_mappings['<Tab>'] = nil
+cmp_mappings['<S-Tab>'] = nil
+
+lsp.setup_nvim_cmp({
+  mapping = cmp_mappings
+})
+
+lsp.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
+
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end)
+
+lsp.setup()
