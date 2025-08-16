603478fdddf726420b996cfc3ebfdd97021a1b08
Matheus Tanaka <matheustanakanog@gmail.com>
2025-08-16 00:29:37 -0300

    add vim diagnostic for errors

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -47,9 +47,21 @@ opt.wrap = false -- Disable line wrap
 -- Fix markdown indentation settings
 vim.g.markdown_recommended_style = 0
 
--- vim.o.background = "dark" -- or "dark" for light mode
--- vim.cmd([[colorscheme gruvbox]])
+vim.o.background = "dark" -- or "dark" for light mode
+vim.cmd([[colorscheme gruvbox]])
 -- vim.cmd([[colorscheme rose-pine]])
 -- vim.cmd([[colorscheme catppuccin-mocha]])
-vim.cmd([[colorscheme carbonfox]]) -- I was using this one
+-- vim.cmd([[colorscheme carbonfox]]) -- I was using this one
 -- vim.cmd([[colorscheme eva01-LCL]])
+
+-- Configure diagnostic error
+vim.diagnostic.config({
+	signs = true,
+	underline = true,
+	virtual_text = {
+		source = "always",
+	},
+	float = {
+		source = "always",
+	},
+})
